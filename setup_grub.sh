ee23c3467b2908b85133619ecfd8fe44de2d1b02
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-07-29 11:13:02 +0500

    feat: remove theme after isntallation

--- setup_grub.sh
+++ setup_grub.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/yeyushengfan258/Particle-circle-grub-theme.git
-cd Particle-circle-grub-theme
+cd Particle-circle-grub-theme/
 sudo ./install.sh -t window
-
+cd ../
+rm -rf Particle-circle-grub-theme
