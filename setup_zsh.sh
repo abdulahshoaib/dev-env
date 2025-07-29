041a3d0437a4fbd6ca63b7b4168a30318e113010
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-07-29 14:33:58 +0500

    feat: setup zshrc automated setup

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,3 +1,37 @@
 #!/usr/bin/env bash
 
-## setup zsh with plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+tee "$HOME/.zshrc"> /dev/null <<EOF
+export ZSH="$HOME/.oh-my-zsh"
+
+ZSH_THEME="robbyrussell"
+
+plugins=(
+  git
+  golang
+  docker
+  git-commit
+  sudo
+  themes
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+)
+
+source $ZSH/oh-my-zsh.sh
+
+export XDG_CURRENT_DESKTOP=sway
+export XDG_SESSION_TYPE=wayland
+export QT_QPA_PLATFORM=wayland
+export SDL_VIDEODRIVER=wayland
+export _JAVA_AWT_WM_NONREPARENTING=1
+export PATH="/home/vexatious/.local/bin:$PATH"
+
+# bun completions
+[ -s "/home/vexatious/.oh-my-zsh/completions/_bun" ] && source "/home/vexatious/.oh-my-zsh/completions/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+EOF
